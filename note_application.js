8176ebb7a5a8787baec38e309a51e35dc4dc0995
kigold <kinglseybox@yahoo.com>
2016-05-17 16:22:00 +0100

    add list notes functionality

--- note_application.js
+++ note_application.js
@@ -2,10 +2,18 @@ function NotesApplication(author){
 	this.author = author;
 	this.notes = [];
 	//methods
+	//create
 	this.create = function(note){
 		this.notes.push(note);
-		console.log(this.notes);
+		//console.log(this.notes);
 
+	};
+	//listNotes
+	this.listNotes = function(){
+		for (var i=0; i<this.notes.length; i++){
+			result = "Note ID: "+ i + "\n" + this.notes[i] + "\n\n By Author " + this.author;
+			console.log(result);
+		};
 	};
 }
 
@@ -13,3 +21,4 @@ function NotesApplication(author){
 var n = new NotesApplication("kingsley");
 n.create("This is my 1st Note Content");
 n.create("This is my 2nd Note Content");
+n.listNotes();
